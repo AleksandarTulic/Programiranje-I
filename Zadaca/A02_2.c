c88ca0b49109502127d6d7aa0dc1eeaade845324
Aleksandar <39736298+Tula99@users.noreply.github.com>
2018-11-11 12:45:09 +0100

    Create A02_2.c

---
+++ Zadaca/A02_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    scanf("%d", &a);
+
+    printf("Povrsina je: %d\n", a*a*6);
+    printf("Zapremina je: %d", a*a*a);
+    return 0;
+}
