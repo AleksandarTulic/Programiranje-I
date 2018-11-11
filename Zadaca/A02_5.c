bb82a7702161470c2b4b07b1f0f063963a616b48
Aleksandar <39736298+Tula99@users.noreply.github.com>
2018-11-11 12:53:28 +0100

    Create A02_5.c

---
+++ Zadaca/A02_5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    float a;
+    scanf("%f", &a);
+    printf("Temparatura u Kelvinu je: %.2f", a+273.15);
+    return 0;
+}
