1c72f9acd03b2e025b950f2fa160ca1c509a4405
Aleksandar <39736298+Tula99@users.noreply.github.com>
2018-11-21 17:15:17 +0100

    Create 2014_A_2.c

---
+++ vjezba/2014_A_2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define PI 3.14159
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int suma = 0;
+    int br = 0;
+    int broj = n;
+
+    while ( broj )
+    {
+        broj/=10;
+        br++;
+    }
+
+    int i;
+    broj=n;
+    while ( broj )
+    {
+        int f = 1;
+        for (i=1;i<=br;i++) f*=(broj%10);
+        broj/=10;
+
+        suma+=f;
+    }
+
+    if ( suma == n ) printf("GOOD");
+    else printf("NO");
+    return 0;
+}
