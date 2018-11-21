ecae041e768efdbe4a1b1a1613a3470dd54f6033
Aleksandar <39736298+Tula99@users.noreply.github.com>
2018-11-21 16:38:57 +0100

    Update 2014_A_3.c

--- vjezba/2014_A_3.c
+++ vjezba/2014_A_3.c
@@ -9,7 +9,8 @@ int main()
     double t = PI/2;
     double pov1 = cos(0) * t;
     double pov2 = 0.0;
-    int br = 2;
+    int br = 1;
+    int kol = 1;
     t/=2;
 
     while ( fabs(pov1-pov2) > 0.0001 )
@@ -17,12 +18,14 @@ int main()
         pov2 = pov1;
         pov1 = 0.0;
         int i;
-        for (i=1;i<=br;i++) pov1+=cos(-PI/2 + i*t) * t;
+        for (i=1;i<=kol;i++) pov1+=cos(-PI/2 + i*t) * t;
 
         t/=2;
-        br+=2;
+        kol=1;
+        for (i=1;i<=br;i++) kol*=2;
+        br++;
     }
 
-    printf("%0.5lf", pov1);
+    printf("%0.4lf", pov1);
     return 0;
 }
